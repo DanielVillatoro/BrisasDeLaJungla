5178e06a8cea0d2db94c8c7a1fb59d156a112cec
DanielVillatoro <danielvillatoroc@gmail.com>
2021-12-26 16:30:18 -0600

    Cambios

--- wwwroot/js/Login.js
+++ wwwroot/js/Login.js
@@ -1,1 +1,28 @@
-﻿
+﻿const Login = () => {
+    var settings = {
+        "url": "http://localhost:8080/api/usuarios",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "usuario": $("#user").val(),
+            "clave": $("#pass").val()
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        if (response.length > 0) {
+            window.location.href = "http://localhost:1411/Inicio";
+        }
+        else {
+            alert("Usuario incorrecto");
+        }
+    });
+
+
+
+}
+
